3ea29c3e7aa6940c7cb4caf44207a5cba6fbdcb4
zjl <644134599@qq.com>
2018-07-22 10:31:12 +0800

    ADD 7.22

--- swiftlecture02/ViewController.swift
+++ swiftlecture02/ViewController.swift
@@ -13,8 +13,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("hello,swift")
+        
+        var name: String = "anne"
+        let fullname=name + "zhao"
+        print(fullname)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
