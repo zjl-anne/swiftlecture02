ee90d05318f95bb51fbf375040dd4ca5ec35e312
zjl <644134599@qq.com>
2018-07-22 16:43:31 +0800

    add 22

--- swiftlecture02/ViewController.swift
+++ swiftlecture02/ViewController.swift
@@ -22,15 +22,37 @@ class ViewController: UIViewController {
         let fullname=name + "zhao"
         
         lbName?.text=fullname
-       
+        let english=["A","B","C"]
+        var ints:[Int]=[1,2,3]
+        var float:Array<Float> = Array(repeating:0,count:5)
         
+       ints.sort()
+        
+        let dic = [1:"A",2:"C",3:"B"]
+        for (a,b) in dic{
+            print("\(a):\(b)")
+        }
+        let dicsorted = dic.sorted(by: {$0.key<$1.key})
+        for(a,b) in dicsorted{
+            print("\(a):\(b)")
+    }
+        var times : Int = 0
+       
+        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (smalltimer) in
+            times = times + 1
+            print("timer:\(smalltimer.isValid)")
+        }
+     self.add(first: 1, second: 2)
     }
     
     override func didReceiveMemoryWarning() {
+        
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func add(first:Int ,second:Int) -> Int  {
+        return first+second
+    }
 
 }
-
